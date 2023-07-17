157e7a3efc7ce75b18773c974b99e08e8e5eca88
ciolfi <ciolfi2@gmail.com>
2023-07-17 14:32:51 -0400

    Correct next.config.js/add modal

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.experiments.topLevelAwait = true ;
-    return config;
-  }
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+//   webpack: (config) => {
+//     config.experiments.topLevelAwait = true ;
+//     return config;
+//   }
+// }
 
 const withPWA = require('next-pwa')({
   dest: 'public'
