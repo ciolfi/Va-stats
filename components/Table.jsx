e88d0ec1c664c6a64c931b0e0bc0263b031ded47
ciolfi <ciolfi2@gmail.com>
2023-07-24 20:41:28 -0400

    Try semi-colon issue - Table.jsx: 119

--- components/Table.jsx
+++ components/Table.jsx
@@ -116,7 +116,7 @@ export default function Table({ columns, tableData, isDelete, onDeleteClick, isE
 									}
 								</th>
 							);
-						})};
+						})}
 						{/* {isDelete || isEditable? <th width={'6%'}>Actions</th> : <></>} */}
 						{isDelete || isEditable? <th width={'8%'}>Actions</th> : <></>}
 					</tr>
