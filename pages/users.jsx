a0d5611a97ff0d7cfed3f61a68de8833a9bc7baa
ciolfi <ciolfi2@gmail.com>
2023-10-07 16:14:38 -0400

    Mod Add Staff pg

--- pages/users.jsx
+++ pages/users.jsx
@@ -218,7 +218,7 @@ export default function Page() {
 
                 {/* ---------- CSV Download button ---------------- */}
                 <Link className={styles.csvbutton} href={"https://visionaid.dreamhosters.com/csv/staff.php"} legacyBehavior>
-                {/* <Link className={styles.csvbutton} href={"https://visionaid.dreamhosters.com/csv"} legacyBehavior> */}
+                  {/* <Link className={styles.csvbutton} href={"https://visionaid.dreamhosters.com/csv"} legacyBehavior> */}
                   <a target="_blank" className={styles.csvbutton}>Staff CSV</a>
                 </Link>
               </p>
@@ -228,31 +228,107 @@ export default function Page() {
                 {showForm ?
                   // <div className={styles.cardcoursesform}>
                   <div className={styles.addstaffform}>
-                    <h2>Add New Staff Member &rarr;</h2><br />
+                    <h2 class={styles.addnewstaffmember}>Add New Staff Member &rarr;</h2><br />
                     <Image alt={'close batches form'} src={'/icons/expand-up.svg'} height={30} width={30} onClick={() => setShowForm(false)} className={styles.collapseButtonUsers} title="Close User Creation Form" />
-                    <form action='/api/usercreate' method='post' onSubmit={() => handleSubmit()}>
-                      <label htmlFor='id'>VisionAid User ID:<span className={styles.requiredelement}>&#42;</span></label>
-                      <input type='text' id='id' name='id' required /><br /><br />
+                    <form action='/api/usercreate' className={styles.addstaffforminputs} method='post' onSubmit={() => handleSubmit()}>
+                      <label htmlFor='id' className={styles.addstafflabel}>VisionAid User ID<span className={styles.requiredelement}>&#42;</span></label>
+                      <input type='text' id='id' name='id' required className={styles.addstaffforminputsbox} /><br /><br />
 
-                      <label htmlFor='email'>Email:<span className={styles.requiredelement}>&#42;</span></label>
-                      <input type='text' id='email' name='email' required /><br /><br />
+                      <label htmlFor='email' className={styles.addstafflabel}>Email<span className={styles.requiredelement}>&#42;</span></label>
+                      <input type='text' id='email' name='email' required className={styles.addstaffforminputsbox} /><br /><br />
 
-                      <label htmlFor='firstname'>First Name:<span className={styles.requiredelement}>&#42;</span></label>
-                      <input type='text' id='firstname' name='firstname' required /><br /><br />
+                      <label htmlFor='firstname' className={styles.addstafflabel}>First Name<span className={styles.requiredelement}>&#42;</span></label>
+                      <input type='text' className={styles.addstaffforminputsbox} id='firstname' name='firstname' required /><br /><br />
 
-                      <label htmlFor='lastname'>Last Name:<span className={styles.requiredelement}>&#42;</span></label>
-                      <input type='text' id='lastname' name='lastname' required /><br /><br />
+                      <label htmlFor='lastname' className={styles.addstafflabel}>Last Name<span className={styles.requiredelement}>&#42;</span></label>
+                      <input type='text' className={styles.addstaffforminputsbox}id='lastname' name='lastname' required /><br /><br />
 
-                      <label htmlFor='role'>Role:<span className={styles.requiredelement}>&#42;</span></label>
-                      <input list='roles' id='role' name='role' required /><br /><br />
-                      <datalist id="roles">
-                        <option value="MANAGEMENT" />
-                        <option value="PM" />
-                        <option value="ADMINISTRATOR" />
-                      </datalist>
+                      {/* NEW */}
+                      <label htmlFor='designation' className={styles.addstafflabel}>Designation<span className={styles.requiredelement}>&#42;</span></label>
+                      <select type='text' className={styles.addstaffforminputsbox}id='designation' name='designation' required >
+                        <option value='trainer'>Trainer</option>
+                        <option value='teachingassistant'>Teaching Assistant</option>
+                        <option value='programcoordinator'>Program Coordinator</option>
+                        <option value='telecaller'>Telecaller</option>
+                        <option value='trainingcoordinator'>Training Coordinator</option>
+                        <option value='programmanager'>Program Manager</option>
+                        <option value='srtrainer'>Sr. Trainer</option>
+                        <option value='ldexecutive'>L & D Executive</option>
+                        <option value='headoftraining'>Head of Training</option>
+                      </select>
+                      <br /><br />
 
-                      <button type='submit' className={styles.studentsformbutton}>Submit</button>&nbsp;&nbsp;
-                      <input type='reset' value='RESET'></input>
+                      {/* NEW */}
+                      <label htmlFor='joindate' className={styles.addstafflabel}>Date of Joining<span className={styles.requiredelement}>&#42;</span></label>
+                      <input type='date' className={styles.addstaffforminputsbox} id='joindate' name='joindate' required />&nbsp;<br />
+
+                      {/* NEW */}
+                      <label htmlFor='mobilenumber' className={styles.addstafflabel}>Mobile Number<span className={styles.requiredelement}>&#42;</span></label>
+                      <input type='tel' className={styles.addstaffforminputsbox} id='mobilenumber' name='mobilenumber' required />&nbsp;<br />
+
+                      {/* NEW */}
+                      <label htmlFor='workbase' className={styles.addstafflabel}>Work Base<span className={styles.requiredelement}>&#42;</span></label>
+                      <input type='text' className={styles.addstaffforminputsbox} id='workbase' name='workbase' required />&nbsp;<br />
+
+                      {/* NEW */}
+                      <label htmlFor='supervisor' className={styles.addstafflabel}>Supervisor<span className={styles.requiredelement}>&#42;</span></label>
+                      <input type='text' className={styles.addstaffforminputsbox} id='supervisor' name='supervisor' required />&nbsp;<br />
+
+                      {/* NEW */}
+                      <label htmlFor='natureofjob' className={styles.addstafflabel}>Nature of Job<span className={styles.requiredelement}>&#42;</span></label>
+                      <select type='text' className={styles.addstaffforminputsbox} id='natureofjob' name='natureofjob' required >
+                        <option value='parttime'>Part time</option>
+                        <option value='fulltime'>Full time</option>
+                      </select>
+                      <br /><br />
+
+                      {/* NEW */}
+                      <label htmlFor='visualacuity' className={styles.addstafflabel}>Visual Acuity<span className={styles.requiredelement}>&#42;</span></label>
+                      <select type='text' className={styles.addstaffforminputsbox} id='visualacuity' name='visualacuity' required >
+                        <option value='blind'>Blind</option>
+                        <option value='lowvision'>Low Vision</option>
+                        <option value='sighted'>Sighted</option>
+                      </select>
+                      <br /><br />
+
+                      {/* NEW */}
+                      <label htmlFor='trainingprogram1' className={styles.addstafflabel}>Training Program 1<span className={styles.requiredelement}>&#42;</span></label>
+                      <input type='text' className={styles.addstaffforminputsbox} id='trainingprogram1' name='trainingprogram1' required />&nbsp;<br />
+
+                      {/* NEW */}
+                      <label htmlFor='trainingprogram2' className={styles.addstafflabel}>Training Program 2<span className={styles.requiredelement}>&#42;</span></label>
+                      <input type='text' className={styles.addstaffforminputsbox} id='trainingprogram2' name='trainingprogram2' required />&nbsp;<br />
+
+                      {/* NEW */}
+                      <label htmlFor='trainingprogram3' className={styles.addstafflabel}>Training Program 3<span className={styles.requiredelement}>&#42;</span></label>
+                      <input type='text' className={styles.addstaffforminputsbox} id='trainingprogram3' name='trainingprogram3' required />&nbsp;<br />
+
+                      {/* Changed options wording; switched to dropdown */}
+                      {/* <label htmlFor='role' className={styles.addstafflabel}>Role<span className={styles.requiredelement}>&#42;</span></label>
+                      <input list='roles' className={styles.addstaffforminputsbox} id='role' name='role' required /><br /><br />
+                      <datalist id='roles'>
+                        <option value='Staff' />
+                        <option value='Management' />
+                        <option value='Administrator' />
+                      </datalist> */}
+
+                      <label htmlFor='role' className={styles.addstafflabel}>Role<span className={styles.requiredelement}>&#42;</span></label>
+                      <select type='text' className={styles.addstaffforminputsbox} id='role' name='role' required>
+                        <option value='staff'>Staff</option>
+                        <option value='management'>Management</option>
+                        <option value='administrator'>Administrator</option>
+                      </select><br /><br />
+
+                      {/* NEW */}
+                      <label htmlFor='active' className={styles.addstafflabel}>Active<span className={styles.requiredelement}>&#42;</span></label>
+                      <input type='text' className={styles.addstaffforminputsbox} id='active' name='active' required />&nbsp;<br />
+
+                      {/* NEW */}
+                      <label htmlFor='action' className={styles.addstafflabel}>Action<span className={styles.requiredelement}>&#42;</span></label>
+                      <input type='text' className={styles.addstaffforminputsbox} id='action' name='action' required />&nbsp;<br /><br /><br />
+
+                      <button type='submit' className={styles.staffformbutton}>Submit</button>&nbsp;&nbsp;
+                      <input type='reset' value='RESET' className={styles.staffformbutton} />
                     </form>
                   </div>
                   : <Button onClick={() => setShowForm(true)} text={'+ New VA Staff'}></Button>
@@ -260,35 +336,35 @@ export default function Page() {
                 <Table columns={usersColumns} tableData={dataResponse} isDelete={true} onDeleteClick={handleDeleteUser} isEditable={true} onEditSave={handleUpdateUser} Title={'Staff List'} />
               </div>
             </main>
-            <footer className={styles.footer}>
-									<Link
-										href='privacypolicy.html'
-										target='_blank'
-										rel='noopener noreferrer'
-									>
-										Privacy
-									</Link>&nbsp;|&nbsp;
-									<Link
-										href='termsofservice.html'
-										target='_blank'
-										rel='noopener noreferrer'
-									>
-										Terms
-									</Link>&nbsp;|&nbsp;
-									<a
-										href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-										target='_blank'
-										rel='noopener noreferrer'
-									>
-										<span className={styles.logo}>
-											Powered by{" "}
-											<Image src='/vercel.svg'
-												alt='Vercel Logo'
-												width={72}
-												height={16} />
-										</span>
-									</a>
-								</footer>
+            {/* <footer className={styles.footer}>
+              <Link
+                href='privacypolicy.html'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                Privacy
+              </Link>&nbsp;|&nbsp;
+              <Link
+                href='termsofservice.html'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                Terms
+              </Link>&nbsp;|&nbsp;
+              <a
+                href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <span className={styles.logo}>
+                  Powered by{" "}
+                  <Image src='/vercel.svg'
+                    alt='Vercel Logo'
+                    width={72}
+                    height={16} />
+                </span>
+              </a>
+            </footer> */}
           </div>
         </>
       );
