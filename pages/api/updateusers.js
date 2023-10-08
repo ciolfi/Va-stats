76646f7b3c2c1c5a17280801067646d28b29ff70
ciolfi <ciolfi2@gmail.com>
2023-10-08 11:13:02 -0400

    Mod 'update users'

--- pages/api/updateusers.js
+++ pages/api/updateusers.js
@@ -2,7 +2,7 @@ import mysql from "mysql2/promise";
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const { id, email, firstname, lastname, role, isactive } = req.body;
+    const { id, email, firstname, lastname, designation, joindate, mobilenumber, workbase, supervisor, natureofjob, visualacuity, trainingprogram1, trainingprogram2, trainingprogram3, role, isactive, action } = req.body;
 
     const dbconnection = await mysql.createConnection({
       host: process.env.MYSQL_HOST,
@@ -18,7 +18,8 @@ export default async function handler(req, res) {
         WHERE id = ?;
       `;
 
-      const values = [email, firstname, lastname, designation, joindate, mobilenumber, workbase, supervisor, natureofjob, visualacuity, trainingprogram1, trainingprogram2, trainingprogram3, role, isactive, action, id];
+      // const values = [email, firstname, lastname, designation, joindate, mobilenumber, workbase, supervisor, natureofjob, visualacuity, trainingprogram1, trainingprogram2, trainingprogram3, role, isactive, action, id];
+      const values = [id, email, firstname, lastname, designation, joindate, mobilenumber, workbase, supervisor, natureofjob, visualacuity, trainingprogram1, trainingprogram2, trainingprogram3, role, isactive, action];
       await dbconnection.execute(query, values);
       res.status(200).json({ message: 'User updated successfully' });
       dbconnection.end();
