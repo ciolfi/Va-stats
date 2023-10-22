8a3424828f5b4144a5f3770e5f36750a3ecf2cf6
ciolfi <ciolfi2@gmail.com>
2023-10-22 17:15:53 -0400

    Style 'add batch form' for desktop & landscape

--- pages/batches copy.jsx
+++ pages/batches copy.jsx
@@ -294,7 +294,7 @@ export default function Page() {
                   {/* LOCAL TESTING LINE BELOW: ADD 'legacyBehavior' ATTRIB FOR LOCAL TESTING */}
                   {/* ---------- CSV Download button ---------------- */}
                   <Link legacyBehavior className={styles.csvbutton} href={"https://visionaid.dreamhosters.com/csv/batches.php"}>
-                    {/* <Link legacyBehavior className={styles.csvbutton} href={"https://visionaid.dreamhosters.com/csv"}> */}
+                  {/* <Link legacyBehavior className={styles.csvbutton} href={"https://visionaid.dreamhosters.com/csv"}> */}
                     <a target="_blank" className={styles.csvbutton}>Batches CSV</a>
                   </Link>
                 </p>
@@ -302,93 +302,75 @@ export default function Page() {
                 <div className={styles.gridcourses}>
                   {showForm ?
                     <div className={styles.cardbatchform}>
-                      <h2>Create batch. &rarr;</h2>
+                      <h2>Create batch &rarr;</h2>
                       <Image alt={'close batches form'} src={'/icons/expand-up.svg'} height={30} width={30} onClick={() => setShowForm(false)} className={styles.collapseButtonBatches} title="Close Batches Form" />
                       <form action='/api/batchcreate' method='post' onSubmit={() => handleSubmit()}>
+                        <label htmlFor='coursename'>Course Name:<span className={styles.requiredelement}>&#42;</span></label>
+                        <select name='coursename' id='coursename' style={{ width: '150px' }} required>
+                          {courseOptions}
+                        </select><br /><br />
 
-                        <section className={styles.addbatchformsec1}>
+                        <label htmlFor='batch'>Batch:<span className={styles.requiredelement}>&#42;</span></label>
+                        <input type='text' id='batch' name='batch' required /><br /><br />
 
-                          <label htmlFor='coursename' className={styles.addstafflabel}>Course Name<span className={styles.requiredelement}>&#42;</span></label>
-                          <select name='coursename' id='coursename' className={styles.addstaffforminputsbox} autofocus required>
-                            {courseOptions}
-                          </select><br /><br />
+                        <label htmlFor='coursestart'>Start:<span className={styles.requiredelement}>&#42;</span></label>
+                        <input type='date' id='coursestart' name='coursestart' placeholder="MM/DD/YYYY" required /><br /><br />
 
-                          <label htmlFor='batch' className={styles.addstafflabel}>Batch<span className={styles.requiredelement}>&#42;</span></label>
-                          <input type='text' className={styles.addstaffforminputsbox} id='batch' name='batch' required /><br /><br />
+                        <label htmlFor='courseend'>End:<span className={styles.requiredelement}>&#42;</span></label>
+                        <input type='date' id='courseend' name='courseend' placeholder="MM/DD/YYYY" required /><br /><br />
 
-                          <label htmlFor='coursestart' className={styles.addstafflabel}>Start<span className={styles.requiredelement}>&#42;</span></label>
-                          <input type='date' className={styles.addstaffforminputsbox} id='coursestart' name='coursestart' placeholder="MM/DD/YYYY" required /><br /><br />
+                        <label htmlFor='coursedays'>Days:<span className={styles.requiredelement}>&#42;</span></label>
+                        <div>
+                          <input type='checkbox' id='M' name='coursedays' value="M" />
+                          <label for='M'>M</label>
+                          <input type='checkbox' id='T' name='coursedays' value="T"></input>
+                          <label for='T'>T</label>
+                          <input type='checkbox' id='W' name='coursedays' value="W"></input>
+                          <label for='W'>W</label>
+                          <input type='checkbox' id='Th' name='coursedays' value="Th"></input>
+                          <label for='Th'>Th</label>
+                          <input type='checkbox' id='F' name='coursedays' value="F"></input>
+                          <label for='F'>F</label>
+                          <input type='checkbox' id='Sa' name='coursedays' value="Sa"></input>
+                          <label for='Sa'>Sa</label>
+                          <input type='checkbox' id='Su' name='coursedays' value="Su"></input>
+                          <label for='Su'>Su</label>
+                        </div>
 
-                          <label htmlFor='courseend' className={styles.addstafflabel}>End<span className={styles.requiredelement}>&#42;</span></label>
-                          <input type='date' className={styles.addstaffforminputsbox} id='courseend' name='courseend' placeholder="MM/DD/YYYY" required /><br /><br />
+                        <label htmlFor='coursetimestart'>Start Time:<span className={styles.requiredelement}>&#42;</span></label>
+                        <input type='time' id='coursetimestart' name='coursetimestart' defaultValue='12:00' required /><br /><br />
 
-                          <label htmlFor='coursedays' className={styles.addstafflabel}>Days<span className={styles.requiredelement}>&#42;</span></label>
-                          <span>
-                            <input type='checkbox' id='M' name='coursedays' value="M" />
-                            <label for='M'>M</label>
-                            <input type='checkbox' id='T' name='coursedays' value="T"></input>
-                            <label for='T'>T</label>
-                            <input type='checkbox' id='W' name='coursedays' value="W"></input>
-                            <label for='W'>W</label>
-                            <input type='checkbox' id='Th' name='coursedays' value="Th"></input>
-                            <label for='Th'>Th</label>
-                            <input type='checkbox' id='F' name='coursedays' value="F"></input>
-                            <label for='F'>F</label>
-                            <input type='checkbox' id='Sa' name='coursedays' value="Sa"></input>
-                            <label for='Sa'>Sa</label>
-                            <input type='checkbox' id='Su' name='coursedays' value="Su"></input>
-                            <label for='Su'>Su</label>
-                          </span><br />
+                        <label htmlFor='coursetimeend'>End Time:<span className={styles.requiredelement}>&#42;</span></label>
+                        <input type='time' id='coursetimeend' name='coursetimeend' defaultValue='12:00' required /><br /><br />
 
-                        </section>
+                        <label htmlFor='instructor'>Instructor:<span className={styles.requiredelement}>&#42;</span></label>
+                        <input type='text' id='instructor' name='instructor' required /><br /><br />
 
-                        <section className={styles.addbatchformsec2}>
+                        <label htmlFor='PM'>PM:<span className={styles.requiredelement}>&#42;</span></label>
+                        <input type='text' id='PM' name='PM' required /><br /><br />
 
-                          <label htmlFor='coursetimestart' className={styles.addstafflabel}>Start Time<span className={styles.requiredelement}>&#42;</span></label>
-                          <input type='time' className={styles.addstaffforminputsbox} id='coursetimestart' name='coursetimestart' defaultValue='12:00' required /><br /><br />
+                        <label htmlFor='TA'>Teaching Assistant:<span className={styles.requiredelement}>&#42;</span></label>
+                        <input type='text' id='TA' name='TA' required /><br /><br />
 
-                          <label htmlFor='coursetimeend' className={styles.addstafflabel}>End Time<span className={styles.requiredelement}>&#42;</span></label>
-                          <input type='time' className={styles.addstaffforminputsbox} id='coursetimeend' name='coursetimeend' defaultValue='12:00' required /><br /><br />
+                        <label htmlFor='cost'>Cost:<span className={styles.requiredelement}>&#42;</span></label>
+                        <input type='text' id='cost' name='cost' placeholder='If free, input 0.' required /><br /><br />
 
-                          <label htmlFor='instructor' className={styles.addstafflabel}>Instructor<span className={styles.requiredelement}>&#42;</span></label>
-                          <input type='text' className={styles.addstaffforminputsbox} id='instructor' name='instructor' required /><br /><br />
+                        <label htmlFor='currency'>Currency:<span className={styles.requiredElement}>&#42;</span></label>
+                        <input type="radio" id="rupees" name="currency" value="INR" />
+                        <label for="rupees">INR</label>
+                        <input type="radio" id="usd" name="currency" value="USD" />
+                        <label for="usd">USD</label><br></br>
 
-                          <label htmlFor='PM' className={styles.addstafflabel}>PM<span className={styles.requiredelement}>&#42;</span></label>
-                          <input type='text' className={styles.addstaffforminputsbox} id='PM' name='PM' required /><br /><br />
+                        <label htmlFor='trainingmode'>Mode of Training:<span className={styles.requiredelement}>&#42;</span></label>
+                        <input list='trainingmodes' id='trainingmode' name='trainingmode' required /><br /><br />
+                        <datalist id="trainingmodes">
+                          <option value="VIRTUAL" />
+                          <option value="IN-PERSON" />
+                          <option value="SELF-PACED" />
+                        </datalist>
 
-                          <label htmlFor='TA' className={styles.addstafflabel}>Teaching Assistant<span className={styles.requiredelement}>&#42;</span></label>
-                          <input type='text' className={styles.addstaffforminputsbox} id='TA' name='TA' required /><br />
-
-                        </section>
-
-                        <section className={styles.addbatchformsec3}>
-
-                          <label htmlFor='cost' className={styles.addstafflabel}>Cost<span className={styles.requiredelement}>&#42;</span></label>
-                          <input type='text' className={styles.addstaffforminputsbox} id='cost' name='cost' placeholder='If free, input 0.' required /><br /><br />
-
-                          <label htmlFor='currency' className={styles.addstafflabel}>Currency<span className={styles.requiredElement}></span>
-                            <span style={{ color: 'red', paddingRight: '1em' }}>
-                              &#42;&nbsp;
-                            </span>
-                          </label>
-                          <input type="radio" id="rupees" name="currency" value="INR" />
-                          <label for="rupees">INR</label>
-                          <input type="radio" id="usd" name="currency" value="USD" />
-                          <label for="usd">USD</label><br /><br />
-
-                          <label htmlFor='trainingmode' className={styles.addstafflabel}>Mode of Training<span className={styles.requiredelement}>&#42;</span></label>
-                          <input list='trainingmodes' className={styles.addstaffforminputsbox} id='trainingmode' name='trainingmode' required /><br /><br />
-                          <datalist id="trainingmodes">
-                            <option value="VIRTUAL" />
-                            <option value="IN-PERSON" />
-                            <option value="SELF-PACED" />
-                          </datalist><br /><br />
-
-                          <input type='reset' value='Reset' className={styles.staffformbutton} /><br /><br /><br />
-                          <button type='submit' className={styles.staffformbutton}>SUBMIT</button>
-
-                        </section>
-
+                        <button type='submit' className={styles.studentsformbutton}>Submit</button>&nbsp;&nbsp;
+                        <input type='reset' value='RESET'></input>
                       </form>
                     </div>
                     : <Button onClick={() => setShowForm(true)} text={'+ New Batch Form'} className={styles.btnnewbatchform} ></Button>
@@ -426,8 +408,8 @@ export default function Page() {
                   </a>
                 </footer> */}
 
-              </main >
-            </div >
+              </main>
+            </div>
           </>
         );
       }
