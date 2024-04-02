75a8a5e2dbf06f73ba518fe7800bd72d531dde1c
Cindy Qian <cqcq810@gmail.com>
2024-04-01 20:48:59 -0400

    Add 'Batch completion report' button

--- pages/batch/[id].jsx
+++ pages/batch/[id].jsx
@@ -665,6 +665,72 @@ export default function Page() {
     batch.forEach((student) => {updateAttendance(student);});
   };
 
+  function exportToCsv(filename, rows) {
+    var processRow = function (row) {
+        var finalVal = '';
+        for (var j = 0; j < row.length; j++) {
+            var innerValue = row[j] === null ? '' : row[j].toString();
+            if (row[j] instanceof Date) {
+                innerValue = row[j].toLocaleString();
+            };
+            var result = innerValue.replace(/"/g, '""');
+            if (result.search(/("|,|\n)/g) >= 0)
+                result = '"' + result + '"';
+            if (j > 0)
+                finalVal += ',';
+            finalVal += result;
+        }
+        return finalVal + '\n';
+    };
+
+    var csvFile = '';
+    for (var i = 0; i < rows.length; i++) {
+        csvFile += processRow(rows[i]);
+    }
+
+    var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+    if (navigator.msSaveBlob) { // IE 10+
+        navigator.msSaveBlob(blob, filename);
+    } else {
+        var link = document.createElement("a");
+        if (link.download !== undefined) { // feature detection
+            // Browsers that support HTML5 download attribute
+            var url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", filename);
+            link.style.visibility = 'hidden';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    }
+  }
+
+  const batchCompletionReportHandler = () => {
+    let rows = [];
+    attendanceData.forEach((student) => {
+      let studentRow = [];
+      studentRow.push(student.name);
+      let totalAttendance = 0;
+      let totalClasses = 0;
+      attendanceColumn.forEach((column) => {
+        if (column.accessor !== 'name' && column.accessor !== 'percent') {
+          if (student[column.accessor] === 1) {
+            totalAttendance++;
+          }
+          if (student[column.accessor] !== 3) {
+            totalClasses++;
+          }
+        }
+      });
+      studentRow.push(totalAttendance);
+      studentRow.push(totalClasses);
+      studentRow.push((totalAttendance / totalClasses) * 100);
+      rows.push(studentRow);
+    });
+    exportToCsv('batch_completion_report.csv', studentRow);
+  }
+
   useEffect(() => {
     setAttendanceColumn(() => {
       return generateColumnsFromDate();
@@ -835,7 +901,7 @@ export default function Page() {
                 <button name="grades" className={styles.addButton} onClick={(e) => batchPageLayoutHandler(e)} >
                   Batch Grades
                 </button>
-                <button name="grades" className={styles.addButton} onClick={(e) => alert("I don't know what to do")} >
+                <button name="grades" className={styles.addButton} onClick={(e) => batchCompletionReportHandler()} >
                   Batch Completion report
                 </button>
                 {(userResponse.role != 'STAFF') ?
